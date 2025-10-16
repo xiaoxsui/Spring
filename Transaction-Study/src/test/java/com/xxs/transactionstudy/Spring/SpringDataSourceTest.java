6a031bb5c8766d570c9971dd2bee0c66b558a042
xiaoxiaosui <1057379513@qq.com>
2025-10-16 14:14:26 +0800

    学习使用jdbcTemplate进行数据库增删改操作

--- Transaction-Study/src/test/java/com/xxs/transactionstudy/Spring/SpringDataSourceTest.java
+++ Transaction-Study/src/test/java/com/xxs/transactionstudy/Spring/SpringDataSourceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
@@ -17,5 +18,11 @@ public class SpringDataSourceTest {
     @Test
     void contextLoads(@Autowired DataSource dataSource) throws SQLException {
         System.out.println(dataSource.getConnection());
+    }
+
+    @Test
+    void TestJdbcTemplate(@Autowired JdbcTemplate jdbcTemplate) throws SQLException, ClassNotFoundException {
+        //可以直接使用update语句实现数据库的增删改
+        jdbcTemplate.update("insert into test_user(id,name) values (?,?)","2","xxs");
     }
 }
--- Transaction-Study/src/test/java/com/xxs/transactionstudy/TransactionStudyApplicationTests.java
+++ Transaction-Study/src/test/java/com/xxs/transactionstudy/TransactionStudyApplicationTests.java
@@ -3,9 +3,12 @@ package com.xxs.transactionstudy;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.List;
 
 @SpringBootTest
 class TransactionStudyApplicationTests {
@@ -15,4 +18,32 @@ class TransactionStudyApplicationTests {
         System.out.println(dataSource.getConnection());
     }
 
+    //添加操作
+    @Test
+    void TestJdbcTemplate(@Autowired JdbcTemplate jdbcTemplate) throws SQLException, ClassNotFoundException {
+        jdbcTemplate.update("insert into test_user(id,name) values (?,?)","2","xxs");
+    }
+
+    //更改操作
+    @Test
+    void TestJdbcTemplateUpdate(@Autowired JdbcTemplate jdbcTemplate) throws SQLException, ClassNotFoundException {
+        String sql = "update test_user set name=? where id=?";
+        jdbcTemplate.update(sql,"xxs",1);
+    }
+
+    //查询操作（单个对象）
+    @Test
+    void TestJdbcTemplateQueryObject(@Autowired JdbcTemplate jdbcTemplate) throws SQLException, ClassNotFoundException {
+        String sql = "Select * from test_user where id=?";
+        User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), 1);
+        System.out.println(user);
+    }
+
+    //查询操作（多个对象）
+    @Test
+    void TestJdbcTemplateQueryList(@Autowired JdbcTemplate jdbcTemplate) throws SQLException, ClassNotFoundException {
+        String sql = "Select * from test_user";
+        List<User> list = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+        System.out.println(list);
+    }
 }
